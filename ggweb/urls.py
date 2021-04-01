69b61a3a040b034f907a37b3f9558bec99bae0cb
Abhishek sharma <52596897+abhishek7389@users.noreply.github.com>
2021-04-01 23:48:57 +0530

    Updated

--- ggweb/urls.py
+++ ggweb/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf import settings
-from djandoconf.urls.static import static
+from django.conf.urls.static import static
 from ggwebapp.views import test
 
 urlpatterns = [
